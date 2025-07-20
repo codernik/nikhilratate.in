0637a6e1734d60ac8e76cd298e2cce54b9d1875e
Nikhil <nikhilratate@gmail.com>
2025-07-21 00:04:20 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [react()],
   build: {
     outDir: "dist",
   },
